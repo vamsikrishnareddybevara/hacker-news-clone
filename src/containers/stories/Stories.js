ebf0d14bf3c5c588c081e270649abb6834eca375
vamsi krishna reddy bevara <vamsikrishnareddybevara@vamsis-MacBook-Pro.local>
2022-04-18 19:31:59 +0530

    improved infinite scroll behaviour

--- src/containers/stories/Stories.js
+++ src/containers/stories/Stories.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import { getStoryIds } from "../../utils/fetchStories";
 import Story from "./../../components/story/Story";
-import { useInfiniteScroll } from "./../../hooks/useInfiniteScroll";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Spin } from "antd";
+const STORY_INCREMENT = 30;
 
 export const Stories = ({ category }) => {
   const [storyIds, setStoryIds] = useState([]);
-  const { count } = useInfiniteScroll();
   const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(STORY_INCREMENT);
+  const loadMoreStories = () => {
+    setCount((preValue) => preValue + STORY_INCREMENT);
+  };
   useEffect(() => {
     setLoading(true);
     getStoryIds(category).then((data) => {
@@ -24,9 +28,21 @@ export const Stories = ({ category }) => {
   }
   return (
     <div>
-      {storyIds.slice(0, count).map((storyId) => (
-        <Story storyId={storyId} key={storyId} />
-      ))}
+      <InfiniteScroll
+        dataLength={count}
+        next={loadMoreStories}
+        hasMore={true}
+        loader={<Spin size="large" />}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {storyIds.slice(0, count).map((storyId) => (
+          <Story storyId={storyId} key={storyId} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
